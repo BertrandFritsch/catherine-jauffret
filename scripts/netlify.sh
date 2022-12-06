d97757d98bef7ef0463022442b68d12c0bb24dc2
Bertrand Fritsch <bertrand.fritsch.in@gmail.com>
2022-12-06 21:42:41 +0100

    Bypass installation of pnpm

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -xeuo pipefail
 test "$CI" = true || exit 1
-npm -g install pnpm
 pnpm install --frozen-lockfile --store-dir=node_modules/.pnpm-store
 pnpm run -C ./packages/gatsby build:incremental
 pnpm run -C ./packages/functions functions:build
